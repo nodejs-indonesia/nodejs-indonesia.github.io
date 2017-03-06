0e870d34706aecf3b8aaf8049bb8d83b62a37ba7
Irfan Maulana <irfan.maulana@gdn-commerce.com>
2017-03-06 19:27:05 +0700

    add gulpfile and build pipe

---
+++ Gulpfile.js
@@ -0,0 +1,63 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+var clean = require('gulp-clean');
+var connect = require('gulp-connect');
+
+var inputSass = ['./src/**/*.scss'];
+var outpoutSass = './dist';
+
+var autoprefixerOptions = {
+    browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
+};
+
+gulp.task('clean:css', function () {
+    return gulp.src(['./dist/*.css'], {read: false})
+        .pipe(clean());
+});
+
+gulp.task('sass', ['clean:css'], function () {
+
+	var sassOptions = {
+		errLogToConsole: true,
+		outputStyle: 'compact'
+	};
+
+    return gulp
+        .src(inputSass)
+        .pipe(sass(sassOptions).on('error', sass.logError))
+        .pipe(autoprefixer(autoprefixerOptions))
+        .pipe(gulp.dest(outpoutSass));
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    port:3030,
+    livereload: true
+  });
+});
+ 
+gulp.task('html', function () {
+  gulp.src('./*.html')
+    .pipe(connect.reload());
+});
+
+gulp.task('watch', function() {
+    return gulp
+        .watch(['./src/**/*.scss', './*.html'], ['sass', 'html'])
+        .on('change', function(event) {
+            console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+        });
+});
+
+gulp.task('sass:prod', ['clean:css'], function () {
+    return gulp
+        .src(inputSass)
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(autoprefixer(autoprefixerOptions))
+        .pipe(gulp.dest(outpoutSass));
+});
+
+gulp.task('default', ['clean:css', 'sass', 'connect', 'watch']);
+gulp.task('prod', ['clean:css', 'sass:prod']);
